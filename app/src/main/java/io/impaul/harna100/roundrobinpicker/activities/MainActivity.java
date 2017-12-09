bcc4ebbfca9d0e076cf4575638a7c3ab86ed6299
Paul Harnack <harna100@mail.chapman.edu>
2017-12-08 23:14:47 -0800

    Gets a toggle button working for navigation drawer

--- app/src/main/java/io/impaul/harna100/roundrobinpicker/activities/MainActivity.java
+++ app/src/main/java/io/impaul/harna100/roundrobinpicker/activities/MainActivity.java
@@ -2,20 +2,19 @@ package io.impaul.harna100.roundrobinpicker.activities;
 
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.MotionEvent;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import io.impaul.harna100.roundrobinpicker.R;
 import io.impaul.harna100.roundrobinpicker.adapters.NavListAdapter;
 import io.impaul.harna100.roundrobinpicker.fragments.HomeFragment;
-import io.impaul.harna100.roundrobinpicker.fragments.MyFragment;
 import io.impaul.harna100.roundrobinpicker.interfaces.NavContainerInterface;
 
 public class MainActivity extends NavContainer implements NavContainerInterface {
@@ -25,6 +24,8 @@ public class MainActivity extends NavContainer implements NavContainerInterface 
 	private NavigationView nv_navBar;
 	private DrawerLayout dl_drawer;
 	private RecyclerView rv_navList;
+
+	private ActionBarDrawerToggle drawerToggle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +38,30 @@ public class MainActivity extends NavContainer implements NavContainerInterface 
 		setFragment(HomeFragment.NewInstance(), false);
 
 		dl_drawer.openDrawer(Gravity.START);
+		getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+
 		setUpRecyclerView();
+
+		drawerToggle = new ActionBarDrawerToggle(this,
+														dl_drawer,
+														R.string.drawer_open,
+														R.string.drawer_close){
+			public void onDrawerClosed(View view){
+				super.onDrawerClosed(view);
+				getSupportActionBar().setTitle(R.string.app_name);
+				getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+			}
+			public void onDrawerOpened(View drawerView){
+				super.onDrawerOpened(drawerView);
+				getSupportActionBar().setTitle("Navigation");
+				getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back);
+			}
+		};
+
+		dl_drawer.addDrawerListener(drawerToggle);
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
 	}
 
 
@@ -58,5 +82,14 @@ public class MainActivity extends NavContainer implements NavContainerInterface 
 		rv_navList.setLayoutManager(new LinearLayoutManager(this));
 		rv_navList.setAdapter(new NavListAdapter());
 		rv_navList.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(drawerToggle.onOptionsItemSelected(item)){
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 }
