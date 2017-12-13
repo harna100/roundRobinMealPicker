92590f5f1d9f2af8b893d16d529bcbcc73f31251
Paul Harnack <harna100@mail.chapman.edu>
2017-12-12 17:52:41 -0800

    Adds working place example to LoginActivity (doesn't use the util I built)

--- app/src/main/java/io/impaul/harna100/roundrobinpicker/activities/LoginActivity.java
+++ app/src/main/java/io/impaul/harna100/roundrobinpicker/activities/LoginActivity.java
@@ -1,11 +1,11 @@
 package io.impaul.harna100.roundrobinpicker.activities;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,20 +17,31 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import io.impaul.harna100.roundrobinpicker.R;
 import io.impaul.harna100.roundrobinpicker.SharedPrefSingleton;
 import io.impaul.harna100.roundrobinpicker.models.PlaceModel;
-import io.impaul.harna100.roundrobinpicker.room.AppDatabase;
+import io.impaul.harna100.roundrobinpicker.places.PlaceUtil;
+import io.impaul.harna100.roundrobinpicker.places.models.NearbyRaw;
 import io.impaul.harna100.roundrobinpicker.room.RoomSingleton;
 import io.impaul.harna100.roundrobinpicker.room.models.Place;
 import io.impaul.harna100.roundrobinpicker.room.models.User;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity{
 
+	private static final String TAG = "LoginActivity";
 	// UI references.
 	private AutoCompleteTextView tv_email;
 	private EditText et_password;
@@ -45,10 +56,56 @@ public class LoginActivity extends AppCompatActivity{
 		getReferences();
 		setListeners();
 		fillFields();
+		getPlaces();
 
 //		nukeAll();
 //		createDummyUser();
 //		createDummyPlaces();
+	}
+
+	private void getPlaces() {
+		String baseUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+		String key = "AIzaSyCcEY0R4SXHcTwT7Y76pO2T8XbB7m1o10U";
+		String location = "33.793339,-117.852069";
+		String radius = "5000";
+		String keyword = "restaurant";
+
+		HttpUrl.Builder urlBuilder = HttpUrl.parse(baseUrl).newBuilder();
+		urlBuilder.addQueryParameter("key", key);
+		urlBuilder.addQueryParameter("location", location);
+		urlBuilder.addQueryParameter("radius", radius);
+		urlBuilder.addQueryParameter("keyword", keyword);
+		String fullUrl = urlBuilder.build().toString();
+
+		final OkHttpClient client = new OkHttpClient();
+
+		final Request request = new Request.Builder()
+				.url(fullUrl)
+				.get()
+				.build();
+
+		client.newCall(request).enqueue(new Callback() {
+			@Override
+			public void onFailure(Call call, IOException e) {
+				e.printStackTrace();
+			}
+
+			@Override
+			public void onResponse(Call call, Response response) throws IOException {
+				if(!response.isSuccessful()){
+					Log.d(TAG, "onResponse: failed: " + response);
+				}
+				else{
+					Moshi moshi = new Moshi.Builder().build();
+					JsonAdapter<NearbyRaw> jsonAdapter = moshi.adapter(NearbyRaw.class);
+					NearbyRaw raw = jsonAdapter.fromJson(response.body().source());
+//					Log.d(TAG, "onResponse: resBody" + response.body());
+					Log.d(TAG, "onResponse: raw:" + raw);
+				}
+			}
+		});
+
+
 	}
 
 	private void nukeAll() {
